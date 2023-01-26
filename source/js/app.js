367937f7ed0d58fe81f055af23c9456495b428a6
AlexFGeeR <alexinfoege@gmail.com>
2023-01-27 00:48:25 +0400

    Start making main-slider component

--- source/js/app.js
+++ source/js/app.js
@@ -2,7 +2,32 @@
 
 switch(location.pathname) {
     case '/': {
-        
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
+            },
+            centeredSlides: true,
+            spaceBetween: 10,
+            slidesPerView: 1.5,
+            breakpoints: {
+              1279: {
+                slidesPerView: 1.5,
+              },
+              0: {
+                slidesPerView: 1,
+              }
+            },
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+
+          });
         break;
     }
 }
