d9ca86712e4a6cf7da2bc44acf77a34a537a8e0d
AlexFGeeR <alexinfoege@gmail.com>
2023-02-13 15:11:04 +0400

    Make personalies page

--- source/js/app.js
+++ source/js/app.js
@@ -50,8 +50,24 @@ switch(location.pathname) {
 
       break;
     }
-    case '/personalities.html': {
+    case '/personalies.html': {
+      const contents = [...document.querySelectorAll('.personalies__tabs-content-list-item')];
+      const btns = [...document.querySelectorAll('.personalies__tabs-btn-list-item')];
       
+      for(let i = 0; i < btns.length; i++ ) {
+        btns[i].addEventListener('click', () => {
+          clearContent();
+          const content = document.querySelector(`#personalies__tabs-content-${i+1}`);
+          content.classList.add('personalies__tabs-content-list-item_active');
+        })
+      }
+
+      function clearContent() {
+        for (let content of contents) {
+          content.classList.remove('personalies__tabs-content-list-item_active');
+        }
+      }
+
       break;
     }
 }
