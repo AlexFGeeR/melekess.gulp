1eb15e01ff49d93e3e5024fb85ae2fc5e5ecc97d
AlexFGeeR <alexinfoege@gmail.com>
2023-02-25 00:18:44 +0400

    make gallery page

---
+++ source/js/gallery-nature.js
@@ -0,0 +1,33 @@
+createSlider('gallery-nature__slider', {
+    initialSlide: 0,
+    animationDuration: 500,
+    extendedPagination: true,
+    extType: 1,
+    imgSrc: 
+        [
+            './img/gallery-nature/slide1.jpg',
+            './img/gallery-nature/slide2.jpg',
+            './img/gallery-nature/slide3.jpg',
+            './img/gallery-nature/slide4.jpg',
+            './img/gallery-nature/slide5.jpg',
+            './img/gallery-nature/slide6.jpg',
+            './img/gallery-nature/slide7.jpg',
+            './img/gallery-nature/slide8.jpg',
+            './img/gallery-nature/slide9.jpg',
+            './img/gallery-nature/slide10.jpg',
+
+    ],
+    paginationsTitle: 
+        [
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+            'Природа Димитровграда',
+    ],
+});
